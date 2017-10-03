728d0d994688bd0b8581545bd8817082e7c9e654
Daniel Perez <tuvistavie@gmail.com>
2017-10-03 20:56:42 +0900

    Add tests for JS ast generator

--- ast-generators/javascript/test/index.js
+++ ast-generators/javascript/test/index.js
@@ -1,8 +1,9 @@
 const fs     = require('fs');
 const path   = require('path');
 const expect = require('chai').expect;
+const temp   = require('temp').track();
 
-const astGenerator = require('..');
+const bigcodeAST = require('..');
 
 const FIXTURES_PATH = path.join(__dirname, 'fixtures');
 
@@ -10,36 +11,72 @@ function stripExtension(filename) {
   return filename.replace(path.extname(filename), '');
 };
 
-describe('bigcode-ast', function() {
-  describe('fromString', function() {
-    const asts = {};
-    const sources = {};
+describe('bigcodeAST', function() {
+  const asts = {};
+  const sources = {};
 
-    function generateContent(object, directoryName, options = {}) {
-      const files = fs.readdirSync(path.join(FIXTURES_PATH, directoryName));
-      files.forEach((filename) => {
-        const filepath = path.join(FIXTURES_PATH, directoryName, filename);
-        const key = stripExtension(filename);
-        const content = fs.readFileSync(filepath, 'utf-8');
-        object[key] = options.json ? JSON.parse(content) : content;
-      });
+  function processRawAST(ast) {
+    const result = JSON.parse(ast);
+    if (result[result.length - 1] === 0) {
+      result.splice(result.length - 1, 1);
     }
+    return result;
+  }
 
-    before(function() {
-      generateContent(asts, 'asts', {json: true});
-      generateContent(sources, 'sources');
+  function generateContent(object, directoryName, options = {}) {
+    const files = fs.readdirSync(path.join(FIXTURES_PATH, directoryName));
+    files.forEach((filename) => {
+      const filepath = path.join(FIXTURES_PATH, directoryName, filename);
+      const key = stripExtension(filename);
+      const content = fs.readFileSync(filepath, 'utf-8');
+      object[key] = options.process ? options.process(content) : content;
     });
+  }
 
+  before(function() {
+    generateContent(asts, 'asts', {process: processRawAST});
+    generateContent(sources, 'sources');
+  });
+
+  describe('fromString', function() {
     it('should return js150 format', function() {
       Object.keys(asts).forEach((key) => {
         const expected = asts[key];
-        const result = astGenerator.fromString(sources[key]);
-        if (expected[expected.length - 1] === 0) {
-          expected.splice(expected.length - 1, 1);
-        }
+        const result = bigcodeAST.fromString(sources[key]);
 
         expect(result).to.deep.equal(expected);
       });
+    });
+  });
+
+  describe('fromFile', function() {
+    it('should parse file and return js150 format', function(cb) {
+      const expected = asts['regress-472450-04'];
+      const filepath = path.join(FIXTURES_PATH, 'sources/regress-472450-04.js');
+      bigcodeAST.fromFile(filepath, (err, result) => {
+        expect(result).to.deep.equal(expected);
+        cb(err);
+      });
+    });
+  });
+
+  describe('bigcodeAST', function() {
+    let resultsDir;
+
+    beforeEach(function() {
+      resultsDir = temp.mkdirSync('bigcodeAST');
+    });
+
+    it('should process all files in the directory', function(cb) {
+      const pattern = path.join(FIXTURES_PATH, 'sources/*.js');
+      bigcodeAST({outputDir: resultsDir, files: pattern}, function(err, count) {
+        expect(count).to.equal(4);
+        cb(err);
+      });
+    });
+
+    afterEach(function() {
+      temp.cleanupSync();
     });
   });
 });
